20774ec3f993798be5ee7743e249ba13d4ce9224
Abdullah Hamdy <abdullah.hamdi.1998@gmail.com>
2023-01-03 23:40:16 +0200

    Refine view structure

--- wp-content/plugins/age-attribute/src/view.php
+++ wp-content/plugins/age-attribute/src/view.php
@@ -4,17 +4,6 @@ class AdminView
 {
     public function render_age_field()
     {
-?>
-        <table class="form-table">
-            <tr>
-                <th>
-                    <label for="age">Age</label>
-                </th>
-                <td>
-                    <input type="number" min="1" name="age" id="age">
-                </td>
-            </tr>
-        </table>
-<?php
+        include plugin_dir_path(__FILE__) . '../templates/age-field.php';
     }
 }
---
+++ wp-content/plugins/age-attribute/templates/age-field.php
@@ -0,0 +1,8 @@
+<table class="form-table">
+    <tr>
+        <th><label for="age">Age</label></th>
+        <td>
+            <input type="number" min="1" name="age" id="age">
+        </td>
+    </tr>
+</table>
