d10b5f5ddb797f23435da4fbf23a43bdb2a40b79
Abdullah Hamdy <abdullah.hamdi.1998@gmail.com>
2023-01-04 19:26:24 +0200

    Make only admins able to edit their profiles

--- wp-content/plugins/age-attribute/src/AdminController.php
+++ wp-content/plugins/age-attribute/src/AdminController.php
@@ -10,12 +10,16 @@ class AdminController
     {
         $this->view = new AdminView();
         $this->model = new UserModel();
-        
+
         add_action('edit_user_profile', array($this, 'add_age_field'));
+        add_action('show_user_profile', array($this, 'add_age_field'));
     }
 
-    public function add_age_field()
+    public function add_age_field($user_id)
     {
+        if (!current_user_can('edit_user', $user_id))
+            return false;
+
         $this->view->render_age_field();
     }
 }
