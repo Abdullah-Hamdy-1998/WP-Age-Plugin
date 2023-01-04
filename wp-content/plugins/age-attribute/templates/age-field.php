fb89e1fffd7b9d87eb49cd544f39408f44fe92a1
Abdullah Hamdy <abdullah.hamdi.1998@gmail.com>
2023-01-04 20:13:15 +0200

    Add JavaScript validation for age field

--- wp-content/plugins/age-attribute/templates/age-field.php
+++ wp-content/plugins/age-attribute/templates/age-field.php
@@ -4,6 +4,19 @@
         <td>
             <input type="number" min="1" name="age" id="age" value="<?php echo esc_attr(get_the_author_meta('age', $user->ID)); ?>">
             <p class="description">Please enter the age.</p>
+            <div id="age-error"></div>
         </td>
     </tr>
 </table>
+
+<script>
+    document.getElementById('age').addEventListener('input', function() {
+        document.getElementById('age-error').innerHTML = '';
+        if (this.value <= 0) {
+            document.getElementById('age-error').innerHTML = '<p class="error">Please enter a positive age.</p>';
+            document.getElementById('submit').disabled = true;
+        } else {
+            document.getElementById('submit').disabled = false;
+        }
+    });
+</script>
