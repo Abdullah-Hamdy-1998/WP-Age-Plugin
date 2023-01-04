499826d211531a9f7a9bf970f3ea6b5b233817fc
Abdullah Hamdy <abdullah.hamdi.1998@gmail.com>
2023-01-04 19:44:55 +0200

    Create save_age_field() method to call update_age() method in AdminController

--- wp-content/plugins/age-attribute/src/AdminController.php
+++ wp-content/plugins/age-attribute/src/AdminController.php
@@ -11,8 +11,13 @@ class AdminController
         $this->view = new AdminView();
         $this->model = new UserModel();
 
+        //user
         add_action('edit_user_profile', array($this, 'add_age_field'));
+        add_action('edit_user_profile_update', array($this, 'save_age_field'));
+
+        //admin
         add_action('show_user_profile', array($this, 'add_age_field'));
+        add_action('personal_options_update', array($this, 'save_age_field'));
     }
 
     public function add_age_field($user_id)
@@ -21,5 +26,14 @@ class AdminController
             return false;
 
         $this->view->render_age_field();
+    }
+
+    public function save_age_field($user_id)
+    {
+
+        if (!current_user_can('edit_user', $user_id))
+            return false;
+
+        $this->model->update_age($user_id, $_POST['age']);
     }
 }
